1cf95b224b43db0e714f3b3460c242839162df20
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-05 19:35:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(deli)
    if deli.length == 0
      puts "The line is currently empty."
     else
-      puts line = "The line is currently:"
+      line = "The line is currently:"
       deli.each_with_index(1) do |name, i|
         line << "#{i}. #{name}"
     end

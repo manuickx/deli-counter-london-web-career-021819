d741bdbc735ef84525a38178b11ff5f07a871a2d
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-05 19:30:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,5 @@
-def line(array)
-  line = []
-   if line.length == 0
+def line(deli)
+   if deli.length == 0
      puts "The line is currently empty"
     else
       puts "There are #{line.length} people in line."
